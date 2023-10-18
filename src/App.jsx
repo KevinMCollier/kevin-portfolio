f684378eef8e6aed87340ee6aba02e530485d65d
kevin collier <collier.kevin.m@gmail.com>
2023-10-18 19:05:25 +0900

    copy-pasted from app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 // import React from "react";
 import './App.css'
+import Banner from "./components/Banner";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import About from "./components/About";
 
 function App() {
 
   return (
     <>
-      <div className="App">
+      <div className="App text-gray-800 bg-gray-100 body-font">
+        <Navbar />
+        <Banner />
+        <Projects />
+        <About />
+        <Contact />
       </div>
     </>
   )
