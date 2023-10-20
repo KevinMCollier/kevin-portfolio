580e3d2cd3366954ad01d7212b8c07f66e8a86f2
kevin collier <collier.kevin.m@gmail.com>
2023-10-20 12:22:46 +0900

    changed banner to deep-cyan

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,9 @@
 export default function Banner() {
   return (
-    <section id="banner">
+    <section id="banner" className="bg-deep-cyan">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-100">
             I&apos;m Kevin and I build apps.
             <br className="hidden lg:inline-block" /> I&apos;m driven to improve the way we learn through technology.
           </h1>
