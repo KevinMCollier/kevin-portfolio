eb70b59022cf42b8d4868a7386ba8b9a75b3c1fa
kevin collier <collier.kevin.m@gmail.com>
2023-10-20 16:47:24 +0900

    adjusted length of banner container and made font larger

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,9 @@
 export default function Banner() {
   return (
-    <section id="banner" className="bg-deep-cyan">
+    <section id="banner" className="bg-deep-cyan h-screen">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-100">
+          <h1 className="title-font sm:text-6xl text-3x sm:mt-5 font-bold text-gray-100 tracking-wide">
             I&apos;m Kevin and I build apps.
             <br className="hidden lg:inline-block" /> I&apos;m driven to improve the way we learn through technology.
           </h1>
@@ -14,7 +14,7 @@ export default function Banner() {
             When I am not coding, you can find me at one of my favorite restaurants across Tokyo.
           </p> */}
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        <div className="sm:mt-5 lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <img
             className="object-cover object-center rounded"
             alt="hero"
