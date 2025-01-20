42da9817e8c1b0b1b2c616f6a53bb0031ee4a103
kevin collier <collier.kevin.m@gmail.com>
2025-01-20 16:10:45 +0900

    Updates content in bio

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 // import React from "react";
-import Banner from "./components/Banner";
-import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
 import About from "./components/About";
-import Footer from "./components/Footer";
+import './App.css';
 
 function App() {
 
   return (
     <>
-      <div className="App text-gray-800 bg-gray-200 body-font">
-        <Navbar />
-        <Banner />
-        <Projects />
+      <div>
         <About />
-        <Contact />
-        <Footer />
       </div>
     </>
   )
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,85 +1,44 @@
-import { useState, useEffect } from "react";
-import Map from "./Map";
-import { locations } from "../data";
-import { highlights } from "../data";
-import './Global.css';
-
 export default function About() {
-  const [showMap, setShowMap] = useState(false);
-  const [currentLocation, setCurrentLocation] = useState(null);
-
-  useEffect(() => {
-    const randomIndex = Math.floor(Math.random() * locations.length);
-    setCurrentLocation(locations[randomIndex]);
-  }, []);
-
   return (
-    <section id="about" className="bg-sand-leather text-gray-800">
-      <div className="container mx-auto flex px-5 pt-20 pb-10 md:flex-row flex-col items-center">
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-full mb-10 md:mb-0 items-center">
-          <img
-            className="object-cover object-center rounded-full mx-auto w-64 h-64"
-            alt="profile"
-            src="./cropped-profile.jpg"
-          />
-          <div className="container px-5 py-5 mx-auto flex flex-col justify-center">
-            <div className="justify-center text-center">
-              <div className="inline-block mr-10 text-4xl text-forest-green hover:text-deep-cyan align-middle">
-                <a href="https://github.com/KevinMCollier" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
-              </div>
-              <div className="inline-block mr-10 text-4xl text-forest-green hover:text-deep-cyan align-middle">
-                <a href="https://www.linkedin.com/in/kevin-collier/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
-              </div>
-            <div className="inline-block text-white text-sm bg-forest-green hover:bg-deep-cyan rounded py-2 px-4 align-middle">
-              <a href="/Collier_Kevin_Resume.pdf" target="_blank" rel="noopener noreferrer">
-                Resume
-              </a>
-            </div>
-            </div>
-          </div>
-        </div>
-        <div className="lg:flex-grow md:w-1/2 flex flex-col sm:items-start md:mb-0">
-          <h1 className="title-font sm:text-3xl text-l mb-4 font-bold text-gray-800 text-center md:text-left">
-            Career Overview
-          </h1>
-          {highlights.map((highlight, index) => (
-            <div key={index} className="mb-2 text-sm flex w-full sm:w-3/4 border-b border-gray-800 leading-relaxed">
-              <p className="w-1/3">{highlight.period}</p>
-              <p className="w-2/3">{highlight.name}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="container mx-auto flex px-10 md:py-4 md:flex-row flex-col items-center justify-center">
-        <div className="mb-8 leading-loose sm:w-3/4 text-semi-bold">
-          <h1 className="sm:text-3xl text-lg mb-4 font-bold text-gray-800 text-center md:text-left">
-            About Me
-          </h1>
-          <p className="mb-4 sm:text-lg">I&apos;m a Full Stack Web Developer with a passion for learning.</p>
-          <p className="mb-4 sm:text-lg">
-            My interest in software engineering started to blossom as I began looking further into potential solutions to problems in the L&D field. I want to have a career where I can contribute hands-on to solutions in both the education field and beyond. My long term goal is to create apps that improve the way we learn and grow.
+    <div className="about-container">
+      <div className="about-flex">
+        <section className="text-section">
+          <p>
+            Growing up in Arizona, my first job was to wash dishes at a Thai restaurant in downtown Tucson. I was often alone, working in the back of the restaurant. I could have felt lonely, but instead I developed meaningful relationships with the servers and chefs. They would come in, vent about a customer, share a laugh, and in those moments I would remember that I was part of a larger team. Washing dishes could have been just a mundane part-time job, but instead it was a lesson in building teams and creating a sense of responsibility towards one another.
           </p>
-          <p className="mb-4 sm:text-lg">
-            Currently, I&apos;m working on developing apps for various projects while also pursuing a full-time role in web development.
+          <br/>
+          <p>
+            That early experience has guided my life and career. From leading classrooms of young students in Japan, to developing recruitment and onboarding departments, to learning about the challenges facing software engineers, I’ve always focused on creating connected and cohesive teams. Along the way, I’ve discovered that the right environment, combined with the right people, can inspire incredible growth and innovation.
           </p>
-          <p className="mb-4 sm:text-lg">
-            I&apos;m originally from Arizona in the United States, but I&apos;ve been living overseas for the past 12 years - one year in the UK, one year in Sweden, and 10+ years in Japan.
-            I&apos;m currently living in Tokyo with my wife and our toy poodle.
-            When I&apos;m not coding, you can find me walking my dog or eating at one of my favorite restaurants around Tokyo.
+          <br/>
+          <p>
+            My goal is to build and support the teams that will do great work together.
           </p>
-          <p className="sm:text-lg">Here is one of my favorites 👉 <a href="#" className="text-lg ml-3 font-semibold cursor-pointer pulse-text" onClick={(e) => { e.preventDefault(); setShowMap(!showMap); }}>{showMap ? "Hide map" : (currentLocation && currentLocation.description + ", anyone?")}</a></p>
 
-        </div>
-      </div>
-        {showMap && <div className="container mx-auto flex px-10 md:flex-row flex-col items-center justify-center sm:w-3/4">
-          <Map
-            lat={currentLocation && currentLocation.coordinates.lat}
-            lng={currentLocation && currentLocation.coordinates.lng}
-            address={currentLocation && currentLocation.address}
-            name={currentLocation && currentLocation.name}
-            link={currentLocation && currentLocation.link}
+        <div className="signature-container">
+  <span className="contact-text">
+    Contact:
+    <a
+      href="https://www.linkedin.com/in/kevin-collier/"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="linkedin-link"
+    >
+      LinkedIn
+    </a>
+  </span>
+  <h2>- Kevin M. Collier</h2>
+</div>
+
+        </section>
+        <aside className="image-section">
+          <img
+            src="src/kevin-standing-profile.JPG"
+            alt="Profile Picture"
+            className="profile-pic"
           />
-        </div>}
-    </section>
+        </aside>
+      </div>
+    </div>
   );
 }
